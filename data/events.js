c8ff5e2ee13075d2d1f73250f2b1232bda35f4b2
Megha Albin <megha.albin@edu.futuregames.se>
2025-03-02 22:46:00 +0100

    fizx

--- data/events.js
+++ data/events.js
@@ -3,12 +3,12 @@
 export const allEvents = [
     {
         name: "workshops",
-        img: "/images/workshop2.jpg",
+        img: "/images/workshop.jpg",
         description: "Learn from top dancers!"
     },
     {
         name: "competitions",
-        img: "/images/dance4.jpg",
+        img: "/images/dance5.jpg",
         description: "Show your skills & win prizes!"
     }
 ];
